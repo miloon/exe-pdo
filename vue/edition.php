b5f339ea24a16d84ba0a6b3fb66abc73db70ce65
miloon <medellyna@gmail.com>
2016-04-24 13:19:55 +0200

    putain y'a un truc qui merde

--- vue/edition.php
+++ vue/edition.php
@@ -1,3 +1,54 @@
+<?php
+// si le formulaire d'édition est vide
+if(empty($_POST['edition'])) {
+    // création d'une variable pour afficher le formulaire
+    $affiche_modif = true;
+
+    $requete = $connexion->prepare("SELECT r.id AS idrecette, r.titre, r.ladesc, r.ladate, p.id AS idpays, p.lintitule AS lepays
+FROM recette r
+  INNER JOIN pays p
+  ON r.pays_id = p.id
+WHERE r.id = $idrecette;
+");
+// exécution de la requête
+    $requete->execute();
+
+    $requetepays = $connexion->prepare("SELECT p.id AS idchoicepays, p.lintitule
+FROM pays p
+ORDER BY p.continent_id ASC, p.lintitule ASC;
+");
+    $requetepays->execute();
+}else { // le formulaire est envoyé
+    $affiche_modif = false; // on cache le formulaire de modif
+    $succes_modif = true; // on affiche le message d'insertion
+    // et on fait les vérif'
+    $letitre = htmlspecialchars(strip_tags(trim($_POST['titre'])),ENT_QUOTES);
+    $ladesc = htmlspecialchars(strip_tags(trim($_POST['ladesc'])),ENT_QUOTES);
+
+    // vérif si l'id du pays est bien un chiffre
+    if (ctype_digit($_POST['idpays'])) {
+        $idpays = $_POST['idpays'];
+    } else {
+        $idpays = 1;
+    }
+
+    try{
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connexion->beginTransaction();
+        $prepare = $connexion->prepare("UPDATE `recette` SET `titre`=:titre,`ladesc`=:ladesc,`ladate`=:ladate,`pays_id`=:idpays WHERE `id`=$idrecette");
+
+        $prepare->bindValue(":titre",$letitre,PDO::PARAM_STR);
+        $prepare->bindValue(":ladesc",$ladesc,PDO::PARAM_STR);
+        $prepare->bindValue(":ladate",$ladate,PDO::PARAM_STR);
+        $prepare->bindValue(":idpays",$idpays,PDO::PARAM_STR);
+
+    }catch (Exception $e){
+        $connexion->rollBack();
+        echo "Erreur : " . $e->getMessage();
+    }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,6 +58,8 @@
 <body>
 <?php require_once "vue/menu.php"; ?>
 <section>
+    <?php if($affiche_modif) {
+    ?>
     <article>
         <h2>Modifier un article</h2>
         <p>
@@ -37,11 +90,17 @@
                 }
 
                 ?>
-            <input type="submit" value='Modifier la recette'/>
+            <input type="submit" name="edition" value='Modifier la recette'/>
         </form>
 
         </p>
     </article>
+    <?php}
+    if($succes_modif) {
+    ?><article>
+        <h2>Article modifié !</h2>
+    </article>
+    <?php } ?>
 </section>
 
 </body>
