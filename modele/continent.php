33d134e2c9a7319db1e9f72d7a7477a6b08fa251
hayat22 <hannou-hayat@outlook.com>
2016-04-27 12:56:01 +0200

    continent fini

--- modele/continent.php
+++ modele/continent.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Stagiaire
- * Date: 22/04/2016
- * Time: 11:01
- */
+
+// requête préparée
+$recup = $connexion->prepare(" SELECT p.lintitule as plintitule,p.ladesc,p.img, r.id as idrecette , r.titre,SUBSTR( r.ladesc,1,100)  as ladescrecette, r.pays_id, c.lintitule as clintitule
+FROM continent c
+  INNER JOIN pays p
+    ON c.id = p.continent_id
+  INNER JOIN recette r
+    ON p.id = r.pays_id
+WHERE c.id = :lid
+ORDER BY r.titre DESC
+LIMIT 3;                 ");
+
+// attribution de valeurs
+$recup->bindValue(':lid',$continent, PDO::PARAM_INT);
+
+
+// exécuter la requête
+$recup->execute();
+
+$recuptous= $recup->fetchAll(PDO::FETCH_OBJ);
+
+$nb = mysqli_num_rows($recuptous);
+
+
+
+
+
--- vue/continent.php
+++ vue/continent.php
@@ -1,7 +1,46 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Stagiaire
- * Date: 22/04/2016
- * Time: 11:01
- */
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>titre</title>
+    <link rel="icon" type="image/png" href="vue/img/favicon.ico"/>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css"/>
+    <!-- Ajout du .js pour le toggle -->
+    <script type="text/javascript" src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+
+</head>
+<body>
+<?php require_once "vue/menu.php"; ?>
+<section>
+    <article>
+
+        <?php
+        foreach ($recuptous as $rec) { ?>
+        <h1><?= $rec->plintitule ?></h1>
+        <p><a><?= $rec->img ?></p></a>
+
+        <?php }
+        ?>
+    </article>
+    <article>
+        <h1><?= $rec->clintitule ?></h1>
+    </article>
+
+    <article>
+        <h2>Nos 3 dernières recettes</h2>
+        <p><?= $nb->idrecette ?></p>
+        <?php
+        foreach ($recuptous as $rec) { ?>
+        <p><?= $rec->titre ?></p>
+        <p><?= nl2br($rec->ladesc) ?>... <a href='?idrecette=<?= $rec->idrecette ?>'>Lire la suite</a>
+            <?php }
+            ?>
+    <article>
+
+    </article>
+
+</section>
+
+</body>
+</html>
